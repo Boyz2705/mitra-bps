d650ebd48624cd2d0ea4ec3924b19bf8dd8d2d25
pramadanif <100917342+pramadanif@users.noreply.github.com>
2024-10-11 11:06:25 +0700

    user

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,20 @@ use App\Http\Controllers\KerjasamaController;
 use App\Http\Controllers\KecamatanController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\JenisController;
+use App\Models\Kerjasama;
 
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [KerjasamaController::class, 'index5']);
 
 Auth::routes();
 
 
 Route::middleware(['auth', 'role:pegawai'])->group(function () {
 
-    Route::get('/mykerjasama', [KerjasamaController::class, 'kerjasama'])->name('mykerjasama');
+    Route::get('/kerjasamaorg', [KerjasamaController::class, 'index4'])->name('kerjasamaorg');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/mitraku', [MitraController::class, 'index2'])->name('mitra.index');
+    Route::post('/', [KerjasamaController::class, 'storeuser'])->name('kerjasama.storeusr');
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
@@ -39,7 +40,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::put('/{id}', [MitraController::class, 'update'])->name('mitra.update'); // Memperbarui mitra
         Route::delete('/{id}', [MitraController::class, 'destroy'])->name('mitra.destroy'); // Menghapus mitra
     });
-    
+
     Route::prefix('surveys')->group(function () {
         Route::get('/', [SurveyController::class, 'index'])->name('surveys.index');
         Route::get('/create', [SurveyController::class, 'create'])->name('surveys.create');
@@ -48,7 +49,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::put('/surveys/{id}', [SurveyController::class, 'update'])->name('surveys.update');
         Route::delete('/{id}', [SurveyController::class, 'destroy'])->name('surveys.destroy');
     });
-    
+
     Route::prefix('subsurvey1s')->group(function () {
         Route::get('/', [Subsurvey1Controller::class, 'index'])->name('subsurvey1s.index');
         Route::get('/create', [Subsurvey1Controller::class, 'create'])->name('subsurvey1s.create');
@@ -57,7 +58,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::put('/subsurvey1s/{id}', [Subsurvey1Controller::class, 'update'])->name('subsurvey1s.update');
         Route::delete('/subsurvey1s/{id}', [Subsurvey1Controller::class, 'destroy'])->name('subsurvey1s.destroy');
     });
-    
+
     Route::prefix('kerjasama')->group(function () {
         Route::get('/', [KerjasamaController::class, 'index'])->name('kerjasama.index'); // Menampilkan daftar kerjasama
         Route::get('/create', [KerjasamaController::class, 'create'])->name('kerjasama.create'); // Form untuk tambah kerjasama baru
@@ -66,7 +67,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::put('/{id}', [KerjasamaController::class, 'update'])->name('kerjasama.update'); // Memperbarui kerjasama
         Route::delete('/kerjasama/kerjasama/{id}', [KerjasamaController::class, 'destroy'])->name('kerjasama.destroy'); // Menghapus kerjasama
     });
-    
+
     Route::prefix('subsurvey2s')->group(function () {
         // Menampilkan daftar Subsurvey2
         Route::get('/', [Subsurvey2Controller::class, 'index'])->name('subsurvey2s.index');
@@ -76,15 +77,15 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::put('/subsurvey2/{id}', [Subsurvey2Controller::class, 'update'])->name('subsurvey2s.update');
         Route::delete('/subsurvey2/{id}', [Subsurvey2Controller::class, 'destroy'])->name('subsurvey2s.destroy');
     });
-    
+
     Route::get('/admin/user', [UserController::class, 'index'])->name('user.index');
     Route::get('/admin/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
     Route::get('/admin/user/create', [UserController::class, 'create'])->name('user.create');
     Route::post('/admin/user/store', [UserController::class, 'store'])->name('user.store');
     Route::put('/admin/user/{id}', [UserController::class, 'update'])->name('user.update');
     Route::delete('/admin/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
-    
-    
+
+
     Route::prefix('admin/kecamatan')->group(function () {
         Route::get('/', [KecamatanController::class, 'index'])->name('kecamatan.index'); // Menampilkan daftar kecamatan
         Route::get('/create', [KecamatanController::class, 'create'])->name('kecamatan.create'); // Form untuk tambah kecamatan baru
@@ -92,11 +93,11 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::get('/{id}/edit', [KecamatanController::class, 'edit'])->name('kecamatan.edit'); // Form untuk edit kecamatan
         Route::put('/{id}', [KecamatanController::class, 'update'])->name('kecamatan.update'); // Memperbarui kecamatan
         Route::delete('/{id}', [KecamatanController::class, 'destroy'])->name('kecamatan.destroy');
-    
+
     });
-    
+
     //Jenis
-    
+
     Route::prefix('admin/jenis')->group(function () {
         Route::get('/', [JenisController::class, 'index'])->name('jenis.index'); // Menampilkan daftar jenis
         Route::get('/create', [JenisController::class, 'create'])->name('jenis.create'); // Form untuk tambah jenis baru
@@ -104,15 +105,11 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::get('/{id}/edit', [JenisController::class, 'edit'])->name('jenis.edit'); // Form untuk edit jenis
         Route::put('/{id}', [JenisController::class, 'update'])->name('jenis.update'); // Memperbarui jenis
         Route::delete('/{id}', [JenisController::class, 'destroy'])->name('jenis.destroy');
-    
+
     });
 });
 
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/kerjasamaku', function () {
     return view('kerjasamaku');
@@ -120,4 +117,4 @@ Route::get('/kerjasamaku', function () {
 
 
 Route::get('/kerjasamaku', [KerjasamaController::class, 'index3'])->name('kerjasamaku.index');
-Route::get('/mitra', [MitraController::class, 'index2'])->name('mitra.index');
+
